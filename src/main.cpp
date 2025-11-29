b8e4712ceec8fafb3b673f3cb8e90eee356a5e17
Florent Cretin <fcretin@z3r3p6.42lyon.fr>
2025-11-29 09:18:56 +0100

    vaisseau qui bouge

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@
 #include "Windown.hpp"
 #include "Debug.hpp"
 
-#define HEIGHT	10
-#define WIDTH	40
+
+#define RUN	    1
+#define EXIT	0
 
 
+int main(int ac, char **av) {
 
-int main(int ac, char **av)
-{
 	(void)av;
 	Debug *ptr = NULL;
 	if (ac > 2) {
@@ -22,59 +22,70 @@ int main(int ac, char **av)
 	if (ac == 2) {
 		ptr = new Debug(av[1]);
 	}
+    bool is_running = RUN;
 
-    // int is_running = 1;
-    initscr();            // Initialise ncurses
-    cbreak();          
-    noecho();             // Désactive l'affichage des input
-	int y = (LINES - HEIGHT);
-	int x = (COLS - WIDTH);
-	// Debug::add_debug_nl("Voila comment ecrire une ligne de debug !");
-	// Debug::add_debug_nl("y: ", y);
-	// Debug::add_debug_nl("x: ", x);
-	
+// 	// Debug::add_debug_nl("Voila comment ecrire une ligne de debug !");
+// 	// Debug::add_debug_nl("y: ", y);
+// 	// Debug::add_debug_nl("x: ", x);
 
-	WINDOW *win = newwin(HEIGHT, WIDTH, y, x);
-	box(win, 0, 0);
-	refresh();
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
 
-    mvwprintw(win, 1, 1, "Hello World !");
-	wrefresh(win);
+    
+    // Taile de la fenetre
+    int h, w;
+    getmaxyx(stdscr, h, w);
+
+	Debug::add_debug_nl("h: ", h);
+	Debug::add_debug_nl("w: ", w);
+    
+    // Création de la fenêtre au centre.
+    int starty = (h - (h - 4)) >> 1;
+    int startx = (w - (w - 4)) >> 1;
+    
+    WINDOW *win = newwin(h - 4, w - 4, starty, startx);
+    box(win, 0, 0);
+    wrefresh(win);
+
+    // Position du "joueur" à l'intérieur de la fenêtre
+    int x = (w - 4) >> 1;
+    int y = (h - 4) - 2;
 
 
+    while (is_running) {
 
-    // curs_set(0);
-    // keypad(stdscr, TRUE);
-    // nodelay(stdscr, TRUE);
+        int ch = getch();
+        if (ch == 'q'|| ch == 27){
+            is_running = EXIT;
+        }
 
-    // if (has_colors()) {
-    //     start_color();
-    //     init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    // }
+        Debug::add_debug_nl("ch: ", ch);
 
+        if (ch == KEY_LEFT && x > 1) x--;
+        else if (ch == KEY_RIGHT && x < w - 6) x++;
+        else if (ch == KEY_UP && y > 1) y--;
+        else if (ch == KEY_DOWN && y < h - 6) y++;
 
-    // while (is_running) {
-    //     int ch = getch();
-    //     if (ch == 'q' || ch == 27)
-    //         is_running = 0;
-		
+        // Effacer l'intérieur de la fenêtre (PAS la bordure)
+        werase(win);
+        box(win, 0, 0);
 
-    //     if (ch == KEY_LEFT) x--;
-    //     if (ch == KEY_RIGHT) x++;
+        // Afficher ton joueur dans la fenêtre
+        mvwprintw(win, y, x, "A");
 
-    //     clear();
-    //     mvwprintw(win, 1, x, "A"); // Joueur
+        // Rafraîchir la fenêtre
+        wrefresh(win);
 
-    //     refresh();
-	// 	wrefresh(win);
+        usleep(16000); // limit 60 fps 16000
+    }
 
-    	// usleep(16000);      // Limite à ~60 FPS
-        usleep(2000000);
-    // }
-
+    delwin(win);
     endwin();
 
-
 	delete ptr;
-	return (0);
+    return 0;
 }
